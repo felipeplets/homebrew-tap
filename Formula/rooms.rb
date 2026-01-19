8b504aa57687812ba217500eee390ca0021afaab
Felipe Plets <felipe@plets.me>
2026-01-19 12:28:33 -0500

    Update rooms.rb

--- Formula/rooms.rb
+++ Formula/rooms.rb
@@ -1,7 +1,7 @@
 class Rooms < Formula
   desc "Terminal UI to manage Git worktrees"
   homepage "https://github.com/felipeplets/rooms"
-  url "https://github.com/felipeplets/rooms/archive/refs/tags/v0.0.1.tar.gz"
+  url "https://github.com/felipeplets/rooms/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "a5ac622885490a590193339053a7d1521217fbbb825ee7f5f84c868731de892d"
   license "MIT"
 
