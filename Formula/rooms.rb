edd617ead3ba072ede98921412a7acf5f8fca91e
Felipe Plets <fsplets@gmail.com>
2026-01-20 16:23:33 +0000

    rooms v0.2.0

--- Formula/rooms.rb
+++ Formula/rooms.rb
@@ -1,8 +1,8 @@
 class Rooms < Formula
   desc "Terminal UI to manage Git worktrees"
   homepage "https://github.com/felipeplets/rooms"
-  url "https://github.com/felipeplets/rooms/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a5ac622885490a590193339053a7d1521217fbbb825ee7f5f84c868731de892d"
+  url "https://github.com/felipeplets/rooms/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "8d89efa942b033642544c1e14a143db885d623294b161933fe1b68efcbb443e0"
   license "MIT"
 
   depends_on "rust" => :build
